1b241c4592f7aae8e75d65456108a1c00f8adce8
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-03-02 14:23:29 +0530

    open api doccumentation begin

--- src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -1,11 +1,27 @@
 package com.eazybytes.accounts;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API documentation",
+				description = "EazyBank Accounts microservice REST API documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Tharindu Jayawardhana",
+						email = "tharinduimalkajayawardhana@gmail.com"
+
+
+				)
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
